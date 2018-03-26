04cc2017d682d48e9dddc8400ced763e7ad84760
jedikarix <karolchecinski@wp.pl>
2018-03-27 00:16:51 +0200

    deploy angular app to heroku

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
+
+app.listen(process.env.PORT || 8080);
+
+
