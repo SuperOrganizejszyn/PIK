64425850195c0a0182d5909c3669b397c6031855
Dram <twiwala@dram.pl>
2018-04-24 07:39:28 +0200

    Client added

---
+++ client/src/app/task-edit/task-edit.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import {TaskService} from "../task.service";
+
+@Component({
+  selector: 'app-task-edit',
+  templateUrl: './task-edit.component.html',
+  styleUrls: ['./task-edit.component.css']
+})
+export class TaskEditComponent implements OnInit {
+  task: any = {};
+
+  sub: Subscription;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private taskService: TaskService) {
+  }
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      const id = params['id'];
+      if (id) {
+        this.taskService.get(id).subscribe((task: any) => {
+          if (task) {
+            this.task = task;
+            this.task.href = task._links.self.href;
+          } else {
+            console.log(`task with id '${id}' not found, returning to list`);
+            this.gotoList();
+          }
+        });
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  gotoList() {
+    this.router.navigate(['/task-list']);
+  }
+
+  save(form: NgForm) {
+    this.taskService.save(form).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error));
+  }
+
+  remove(href) {
+    this.taskService.remove(href).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error));
+  }
+
+}
---
+++ client/src/app/task.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+
+@Injectable()
+export class TaskService {
+
+  constructor(private http: HttpClient) { }
+  public API = '//localhost:8080';
+  public TASK_API = this.API + '/tasks';
+
+  getAll(): Observable<any> {
+    return this.http.get(this.TASK_API + '/all');
+  }
+  get(id: string) {
+    return this.http.get(this.TASK_API + '/' + id);
+  }
+  save(task: any): Observable<any> {
+    let result: Observable<Object>;
+    if (task['href']) {
+      result = this.http.put(task.href, task);
+    } else {
+      result = this.http.post(this.TASK_API, task);
+    }
+    return result;
+  }
+
+  remove(href: string) {
+    return this.http.delete(href);
+  }
+}
